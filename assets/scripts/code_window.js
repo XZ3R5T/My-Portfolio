0192099d3737bc59d09efea8bd66356312851ed5
XZ3R5T <zeruknipz@gmail.com>
2025-02-25 13:55:51 +0700

    feat: add certificates page, update CV, and enhance code animation

--- assets/scripts/code_window.js
+++ assets/scripts/code_window.js
@@ -1,33 +1,47 @@
 const codeLines = [
-    "pwd",
-    "/home/XZ3R5T",
-    "ls -a",
-    ". .. about.txt location.txt contact.txt",
-    "",
-    "cat about.txt",
-    "Hello! I'm a Computer Science Student",
-    "",
-    "cat location.txt",
-    "I'm currently based in Jakarta, Indonesia",
-    "",
-    "cat contact.txt",
-    "You can reach me at:",
-    "Email: madinarawdo@protonmail.com",
-    "LinkedIn: linkedin.com/in/rawdo-madina",
-    "Github: github.com/XZ3R5T"
-  ];
+  "pwd",
+  "/home/XZ3R5T",
+  "ls -a",
+  ". .. about.txt location.txt contact.txt",
+  "",
+  "cat about.txt",
+  "Hello! I'm a Computer Science Student",
+  "",
+  "cat location.txt",
+  "I'm currently based in Jakarta, Indonesia",
+  "",
+  "cat contact.txt",
+  "You can reach me at:",
+  "Email: madinarawdo@gmail.com",
+  "LinkedIn: linkedin.com/in/xz3r5",
+  "Github: github.com/XZ3R5T"
+];
   
   const codeElement = document.getElementById('codeAnimation');
-  
-  function animateCode() {
-    codeLines.forEach((line, index) => {
-        setTimeout(() => {
-            codeElement.textContent += line + '\n';
-            codeElement.scrollTop = codeElement.scrollHeight; 
-        }, 1000 * index); 
+
+  function typeLine(line, delay) {
+    return new Promise((resolve) => {
+      let i = 0;
+      function typeChar() {
+        if (i < line.length) {
+          codeElement.textContent += line.charAt(i);
+          i++;
+          setTimeout(typeChar, 50);
+        } else {
+          codeElement.textContent += '\n';
+          codeElement.scrollTop = codeElement.scrollHeight;
+          resolve();
+        }
+      }
+      typeChar();
     });
   }
-  
+
+  async function animateCode() {
+    for (const line of codeLines) {
+      await typeLine(line, 50);
+    }
+  }
+
   animateCode();
-  
   
